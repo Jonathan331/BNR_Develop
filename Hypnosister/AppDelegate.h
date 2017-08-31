3d17bdca7f41064f0baaae8b04a69859f74e25e6
Jonathan <985532909@qq.com>
2017-08-31 22:34:28 +0800

    add hypnosister

---
+++ Hypnosister/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hypnosister
+//
+//  Created by Jonathan on 2017/8/31.
+//  Copyright © 2017年 China UnionPay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hypnosister/BNRHypnosisView.h
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisView.h
+//  Hypnosister
+//
+//  Created by Jonathan on 2017/8/31.
+//  Copyright © 2017年 China UnionPay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRHypnosisView : UIView
+
+@end
