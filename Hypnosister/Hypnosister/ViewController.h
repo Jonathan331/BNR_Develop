bd3a6651d0ba517952ec59f406d015d00a6dd98d
Jonathan <985532909@qq.com>
2017-08-31 22:45:08 +0800

    add hypnosister

---
+++ Hypnosister/Hypnosister/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hypnosister
+//
+//  Created by Jonathan on 2017/8/31.
+//  Copyright © 2017年 Jonathan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
